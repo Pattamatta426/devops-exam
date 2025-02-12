35c3e281951cd40a3cae662747fa4ee00faf1af9
Aneesh Pattamatta <aneeshpattamatta2910@gmail.com>
2025-02-12 15:48:54 +0530

    terraform

--- subnet.tf
+++ subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "private_subnet" {
   vpc_id                  = data.aws_vpc.vpc.id
-  cidr_block              = "10.0.10.0/24"  
+  cidr_block              = "10.0.23.0/24"  
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = false
 
